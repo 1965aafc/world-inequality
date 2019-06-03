67cc47f5c19526b3d13b42bd3a7833d1039f61da
Erica Bugden <erica.bugden@gmail.com>
2019-06-03 12:05:48 -0400

    Hack to get nav to not stick to top of bars

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -35,7 +35,7 @@ window.onscroll = function() {adjustStickyTabs()};
 var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = navbar.offsetTop + 1;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function adjustStickyTabs() {
